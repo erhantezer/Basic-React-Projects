c58dc8da5c2247e0d20f9fa43aaf350d55c57fb4
Erhan <erhantezer68@gmail.com>
2023-01-30 02:08:22 +0300

    app js fetch data

--- 02-tours/src/App.js
+++ 02-tours/src/App.js
@@ -1,10 +1,55 @@
+import { useEffect, useState } from "react";
+import Tours from "./Tours";
 
+const url = 'https://course-api.com/react-tours-project'
 
 function App() {
+  const [tours, setTours] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchTours = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours)
+    } catch (error) {
+      console.log(error);
+      setLoading(false)
+    }
+  };
+
+  useEffect(() => {
+    fetchTours()
+  }, []);
+
+
   return (
-    <div >
-      
-    </div>
+    <>
+      {loading ? (
+        <main>
+          <div className="loading">
+            <h1>Loading...</h1>
+          </div>
+        </main>
+      ) : (
+        tours.length === 0 ? (
+          <main>
+            <div className='title'>
+              <h2>no tours left</h2>
+              <button className='btn' onClick={() => fetchTours()}>
+                refresh
+              </button>
+            </div>
+          </main>
+        ) : (
+          <main>
+            <Tours />
+          </main>
+        )
+      )}
+    </>
   );
 }
 
