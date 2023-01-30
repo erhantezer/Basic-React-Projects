7171b992242ece9f59c8d7e07428966730587ba6
Erhan <erhantezer68@gmail.com>
2023-01-31 02:31:21 +0300

    updated

--- 03-reviews/src/Review.js
+++ 03-reviews/src/Review.js
@@ -1,9 +1,30 @@
 import React from 'react'
 
 const Review = () => {
-    
+
   return (
-    <div>Review</div>
+    <article className='review'>
+      <div className='img-container'>
+        <img src={null} alt={null} className='person-img' />
+        <span className='quote-icon'>
+          {/* <FaQuoteRight /> */}
+        </span>
+      </div>
+      <h4 className='author'>{null}</h4>
+      <p className='job'>{null}</p>
+      <p className='info'>{null}</p>
+      <div className='button-container'>
+        <button className='prev-btn' onClick={null}>
+          {/* <FaChevronLeft /> */}
+        </button>
+        <button className='next-btn' onClick={null}>
+          {/* <FaChevronRight /> */}
+        </button>
+      </div>
+      <button className='random-btn' onClick={null}>
+        surprise me
+      </button>
+    </article>
   )
 }
 
