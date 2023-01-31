2d79339c28135a248f2b1c8506a242fbadb4691d
Erhan <erhantezer68@gmail.com>
2023-02-01 00:52:49 +0300

    data fetch and filter data

--- 05-menu/src/App.js
+++ 05-menu/src/App.js
@@ -1,10 +1,38 @@
+import items from "./data";
+import {useState} from "react";
 
+
+const allCatogories = ["all", ...new Set(items.map((item) => item.category))] 
+//! aynı catogory leri tekrardan yazmasın diye küme işlemi yaptık
+
+// const numbers = [2,3,4,4,2,3,3,4,4,5,5,6,6,7,5,32,3,4,5]
+// console.log([...new Set(numbers)]) 
+// [2, 3, 4, 5, 6, 7, 32]
 
 function App() {
+const [categories, setCategories] = useState(allCatogories);
+const [menuItems, setMenuItems] = useState(items)
+
+const filterItems = (category) => {
+  if(category === "all") {
+    setMenuItems(items);
+    return;
+  };
+
+  const newItems = items.filter((item) => item.category === category);
+  setMenuItems(newItems)
+}
+
+
   return (
-    <div >
-   
-    </div>
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+      </section>
+    </main>
   );
 }
 
