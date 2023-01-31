848eaa834be1b8fcdbd61bf6a88f658d86b42161
Erhan <erhantezer68@gmail.com>
2023-01-31 14:46:12 +0300

    updated and finished

--- 03-reviews/src/Review.js
+++ 03-reviews/src/Review.js
@@ -1,33 +1,81 @@
-import { useState } from "react"
-import reviews from "./data"
+import { useState } from "react";
+import reviews from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-const [index, setIndex] = useState(0);
-const {id, name, job, image} = reviews[index];
+  const [index, setIndex] = useState(0);
+  const [show, setShow] = useState(false)
+  const { name, job, image, text } = reviews[index];
+  console.log(reviews[0])
 
+  const checkNumber = (number) => {
+    if (number > reviews.length - 1) {
+      return 0;
+    } else if (number < 0) {
+      return reviews.length - 1
+    } else {
+      return number;
+    }
+  }
+
+
+  const nextPerson = () => {
+    setIndex((index) => {
+      const newNext = index + 1;
+      return checkNumber(newNext)
+    })
+  }
+
+  const prevPerson = () => {
+    setIndex((index) => {
+      const newPrev = index - 1
+      return checkNumber(newPrev)
+    })
+  }
+
+  const randomPerson = () => {
+    let randomNum = Math.floor(Math.random() * reviews.length);
+    if (randomNum === index) {
+      randomNum = index + 1;
+    }
+    setIndex(checkNumber(randomNum))
+  }
 
   return (
     <article className='review'>
+
       <div className='img-container'>
-        <img src={null} alt={null} className='person-img' />
+        <img src={image} alt={name} className='person-img' />
         <span className='quote-icon'>
-          {/* <FaQuoteRight /> */}
+          <FaQuoteRight />
         </span>
       </div>
-      <h4 className='author'>{null}</h4>
-      <p className='job'>{null}</p>
-      <p className='info'>{null}</p>
+
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+
+      <p className='info'>
+        {show ? text : `${text.slice(0, 10)}... `}
+        <button onClick={() => setShow(!show)}>
+          {show ? " Show Less" : " Read More"}
+        </button>
+      </p>
+
       <div className='button-container'>
-        <button className='prev-btn' onClick={null}>
-          {/* <FaChevronLeft /> */}
+
+        <button className='prev-btn' onClick={prevPerson}>
+          <FaChevronLeft />
         </button>
-        <button className='next-btn' onClick={null}>
-          {/* <FaChevronRight /> */}
+
+        <button className='next-btn' onClick={nextPerson}>
+          <FaChevronRight />
         </button>
+
       </div>
-      <button className='random-btn' onClick={null}>
+      <button className='random-btn' onClick={randomPerson}>
         surprise me
       </button>
+      
     </article>
   )
 }
