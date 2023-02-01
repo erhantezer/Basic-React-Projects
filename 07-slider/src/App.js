02caec7d3c3a4a79a58698ca88b2cd947be06364
Erhan <erhantezer68@gmail.com>
2023-02-01 23:36:49 +0300

    slider app js edited

--- 07-slider/src/App.js
+++ 07-slider/src/App.js
@@ -1,10 +1,25 @@
+import { useState } from "react";
+import data from "./data";
 
 
 function App() {
-  return (
-    <div>
+  const [people, setPeople] = useState(data);
 
-    </div>
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        {people?.map((person, perIndex) => {
+          const {id, image, name, title, quote} = person;
+          
+        })}
+      </div>
+    </section>
   );
 }
 
