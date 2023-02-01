10c50b06b43cb2c9bef7a06d9fcd49102d458755
Erhan <erhantezer68@gmail.com>
2023-02-01 16:29:53 +0300

    updated app js

--- 06-tabs/src/App.js
+++ 06-tabs/src/App.js
@@ -1,6 +1,24 @@
-
+import axios from "axios";
+import { useState } from "react";
+const url = 'https://course-api.com/react-tabs-project';
 
 function App() {
+  const [tabs, setTabs] = useState([]);
+  const [loading, setLoading] = useState(false)
+
+  const fetchTabs = async () => {
+    setLoading(true)
+    try {
+      const { data } = await axios(url)
+      setTabs(data)
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }
+
+
   return (
     <section>
 
