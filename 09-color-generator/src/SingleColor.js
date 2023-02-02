3ecd074883b7660beb75699a456b3776aed4c0c2
Erhan <erhantezer68@gmail.com>
2023-02-02 22:30:41 +0300

    finished project

--- 09-color-generator/src/SingleColor.js
+++ 09-color-generator/src/SingleColor.js
@@ -1,12 +1,40 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import rgbToHex from './utils';
 
 const SingleColor = ({ rgb, weight, index, hexColor }) => {
-const [alert, setAlert] = useState(false);
+  const [alert, setAlert] = useState(false);
 
-console.log((48).toString(16)) //! hexadesimal şeklinde çevirir ve string yapar
+  console.log((48).toString(16)) //! hexadesimal şeklinde çevirir ve string yapar
+
+  const bcg = rgb.join(',');
+  const hex = rgbToHex(...rgb);
+  const hexValue = `#${hexColor}`
+
+  console.log(bcg);
+  console.log(hex);
+  console.log(hexValue);
+
+  useEffect(() => {
+    let timeout = setTimeout(() => {
+      setAlert(false)
+    }, 3000);
+    return () => clearTimeout(timeout)
+  }, [alert]);
 
   return (
-    <div>SingleColor</div>
+    <article
+      className={`color ${index > 10 && 'color-light'}`}
+      style={{ background: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true)
+        navigator.clipboard.writeText(hexValue)
+      }}
+    >
+
+      <p className='percent-value'>{weight}%</p>
+      <p className='color-value'>{hexValue}</p>
+      {alert && <p className='alert'>copied to clipboard</p>}
+    </article>
   )
 }
 
