b19365d7fad10bc6ecb4606ad4fc98ef30a2df13
Erhan <erhantezer68@gmail.com>
2023-02-02 16:09:14 +0300

    updated

--- 08-lorem-ipsum/src/App.js
+++ 08-lorem-ipsum/src/App.js
@@ -1,10 +1,29 @@
-
+import data from "./data";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div>
+  const [text, setText] = useState([]);
+  const [count, setCount] = useState(0);
 
-    </div>
+
+  const handleSubmit = (e) => {
+    
+  }
+
+  return (
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">Paragraphs:</label>
+        <input 
+        type="number"
+        name="amount"
+        id="amount"
+        onChange={(e) => setCount(e.target.value)}
+        />
+        <button className="btn">generate</button>
+      </form>
+    </section>
   );
 }
 
