30a78356fae92651135aec2c462a1088b2c57718
Erhan <erhantezer68@gmail.com>
2023-02-03 15:03:58 +0300

    updated

--- 10-grocery-bud/src/App.js
+++ 10-grocery-bud/src/App.js
@@ -13,7 +13,11 @@ function App() {
     if(!name) {
       showAlert(true, "danger", "please enter value")
     }
-    setList(name)
+    else {
+      showAlert(true, "success", 'item added to the list');
+      const newItem = { id: new Date().getTime().toString(), title: name}
+      setList([...list, newItem])
+    }
   }
 
 
--- 10-grocery-bud/src/List.js
+++ 10-grocery-bud/src/List.js
@@ -3,7 +3,11 @@ import React from 'react'
 const List = ({items}) => {
     console.log(items)
   return (
-    <div>{items}</div>
+    <div>{items?.map((item) => {
+        return(
+            <p key={item.id}>{item.title}</p>
+        )
+    })}</div>
   )
 }
 
