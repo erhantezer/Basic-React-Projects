db9bd94ee4eee756bfc595cde06dcc3cea4713a5
Erhan <erhantezer68@gmail.com>
2023-02-04 00:12:22 +0300

    updated

--- 10-grocery-bud/src/App.js
+++ 10-grocery-bud/src/App.js
@@ -62,7 +62,7 @@ function App() {
         </div>
       </form>
       <div className='grocery-container'>
-          <List items={list}  />
+          <List items={list} removeItem={removeItem} />
           <button className='clear-btn' onClick={clearList} >
             clear items
           </button>
