13f9db4cee023d05ce2a18c24661508e27e9d1bc
Erhan <erhantezer68@gmail.com>
2023-02-03 14:16:46 +0300

    updated

--- 10-grocery-bud/src/App.js
+++ 10-grocery-bud/src/App.js
@@ -1,29 +1,32 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
+import Alert from "./Alert";
 
 
 function App() {
   const [name, setName] = useState("");
   const [list, setList] = useState(JSON.parse(localStorage.getItem('list')));
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
 
-const handleSubmit = (e) => {
-  e.preventDefault()
-}
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
 
-useEffect(() => {
-  localStorage.setItem("list", JSON.stringify(list))
-},[list])
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list))
+  }, [list])
 
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit} >
+      {alert.show && <Alert {...alert}/>}
         <h3>grocery bud</h3>
         <div className="form-control">
-          <input 
-          type="text"
-          className="grocery"
-          placeholder="e.g. eggs"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e.g. eggs"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
           <button type="submit" className="submit-btn">
             submit
