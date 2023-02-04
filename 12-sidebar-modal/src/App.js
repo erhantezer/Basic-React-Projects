9985ef48ae6de17fc302d90f833d8d8aaeac0688
Erhan <erhantezer68@gmail.com>
2023-02-05 02:19:17 +0300

    app js edited

--- 12-sidebar-modal/src/App.js
+++ 12-sidebar-modal/src/App.js
@@ -1,9 +1,14 @@
+import Home from "./Home";
+import Modal from "./Modal";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
-    <div >
-      
-    </div>
+    <>
+      <Home/>
+      <Modal/>
+      <Sidebar/>
+    </>
   );
 }
 
