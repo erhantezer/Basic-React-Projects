62116fa7c7b7bad4de094ee365bc9cabad8eaccb
Erhan <erhantezer68@gmail.com>
2023-02-05 03:35:54 +0300

    updated

--- 12-sidebar-modal/src/Modal.js
+++ 12-sidebar-modal/src/Modal.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { FaTimes } from 'react-icons/fa'
 
 const Modal = () => {
   return (
-    <div>Modal</div>
+    <div className='modal-overlay'>
+        <div className='modal-container'>
+            <h3>modal content</h3>
+            <button className='close-modal-btn'>
+                <FaTimes/>
+            </button>
+        </div>
+    </div>
   )
 }
 
