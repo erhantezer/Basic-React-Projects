837cac7ffa3d3c5dd0084c58a364e1fa514d951d
Erhan <erhantezer68@gmail.com>
2023-02-06 01:02:49 +0300

    context edited

---
+++ 13-stripe-submenus/src/context.js
@@ -0,0 +1,18 @@
+import { createContext, useContext } from "react";
+
+//! 1 create
+const AppContext = createContext();
+
+//! custom Hook
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+}
+
+export const AppProvider = ({children}) => {
+
+    return(
+        <AppContext.Provider value={{}}>
+            {children}
+        </AppContext.Provider>
+        )
+}
