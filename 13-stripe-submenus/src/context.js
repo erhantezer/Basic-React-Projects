f1472c13743910fb20ca6dd6b0567cff82159039
Erhan <erhantezer68@gmail.com>
2023-02-06 06:31:23 +0300

    context updated

--- 13-stripe-submenus/src/context.js
+++ 13-stripe-submenus/src/context.js
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 //! 1 create
 const AppContext = createContext();
@@ -8,12 +8,36 @@ export const useGlobalContext = () => {
     return useContext(AppContext)
 }
 
-export const AppProvider = ({children}) => {
+export const AppProvider = ({ children }) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
 
+    const openSidebar = () => {
+        setIsSidebarOpen(true)
+    };
 
-    return(
-        <AppContext.Provider value={{}}>
+    const closeSidebar = () => {
+        setIsSidebarOpen(false)
+    };
+
+    const openSubmenu = () => {
+        setIsSubmenuOpen(true)
+    };
+
+    const closeSubmenu = () => {
+        setIsSubmenuOpen(false)
+    };
+
+    return (
+        <AppContext.Provider value={{ 
+        openSidebar, 
+        closeSidebar, 
+        openSubmenu, 
+        closeSubmenu, 
+        isSidebarOpen, 
+        isSubmenuOpen, 
+        }}>
             {children}
         </AppContext.Provider>
-        )
+    )
 }
