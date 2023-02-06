91a4944365487d81315aacafeeb5c2305908848e
Erhan <erhantezer68@gmail.com>
2023-02-07 01:13:41 +0300

    updated

--- 13-stripe-submenus/src/Navbar.js
+++ 13-stripe-submenus/src/Navbar.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import { FaBars } from 'react-icons/fa';
+import logo from "./images/logo.svg"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='nav'>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} alt="logo" className='nav-logo' />
+          <button className='btn toggle-btn'>
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <button className="link-btn">
+              products
+            </button>
+          </li>
+          <li>
+            <button className="link-btn">
+              developers
+            </button>
+          </li>
+          <li>
+            <button className="link-btn">
+              company
+            </button>
+          </li>
+        </ul>
+        <button className='btn signin-btn'>Sign in</button>
+      </div>
+
+    </nav>
   )
 }
 
