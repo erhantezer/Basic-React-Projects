7d934af313e627fc263bf2c2000e0dce4bcb77e3
Erhan <erhantezer68@gmail.com>
2023-02-06 03:47:40 +0300

    app js finished

--- 13-stripe-submenus/src/App.js
+++ 13-stripe-submenus/src/App.js
@@ -1,9 +1,16 @@
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Submenu from "./Submenu";
 
 
 function App() {
   return (
     <>
-
+      <Navbar/>
+      <Sidebar/>
+      <Hero/>
+      <Submenu/>
     </>
   );
 }
