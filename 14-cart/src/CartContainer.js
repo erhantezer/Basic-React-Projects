1d9c23c54158449e40f0dbd8e79bc5225ee5a053
Erhan <erhantezer68@gmail.com>
2023-02-08 22:01:36 +0300

    cartContainer updated

--- 14-cart/src/CartContainer.js
+++ 14-cart/src/CartContainer.js
@@ -1,8 +1,35 @@
-import React from 'react'
+
 
 const CartContainer = () => {
+
+
+
+
+
+  
   return (
-    <div>CartContainer</div>
+    <section className='cart'>
+      {/* cart header */}
+      <header>
+        <h2>your bag</h2>
+      </header>
+      {/* cart items */}
+      <div>
+        
+      </div>
+      {/* cart footer */}
+      <footer>
+        <hr />
+        <div className='cart-total'>
+          <h4>
+            total <span>$</span>
+          </h4>
+        </div>
+        <button className='btn clear-btn'>
+          clear cart
+        </button>
+      </footer>
+    </section>
   )
 }
 
