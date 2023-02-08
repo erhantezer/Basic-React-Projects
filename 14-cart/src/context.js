39d5a4a9735825a852ff94bfd5ca0dd17805196c
Erhan <erhantezer68@gmail.com>
2023-02-08 21:29:24 +0300

    data fetched

--- 14-cart/src/context.js
+++ 14-cart/src/context.js
@@ -1,5 +1,8 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
+import {cartItems} from './data'
+import { reducer } from "./reducer";
 
+const url = 'https://course-api.com/react-useReducer-cart-project'
 
 const AppContext = createContext();
 
@@ -8,10 +11,26 @@ export const useGlobalContext = () => {
 }
 
 
+
+const initialState = {
+    loading: false,
+    cart: cartItems,
+    total: 0,
+    amount: 0,
+}
+
 const AppProvider = ({children}) => {
-    
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+const fetchData = async () => {
+    dispatch({type: "LOADİNG"})
+    const response = await fetch(url);
+    const cart = await response.json();
+    dispatch({type: "DISPLAY_ITEMS", payload: cart})
+}
+
     return (
-        <AppContext.Provider value={{}}>
+        <AppContext.Provider value={{...state, fetchData}}>
             {children}
         </AppContext.Provider>
         )
--- 14-cart/src/data.js
+++ 14-cart/src/data.js
@@ -1,4 +1,4 @@
-export const data= [
+export const cartItems= [
   {
     id: 1,
     title: 'Samsung Galaxy S7',
