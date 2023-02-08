d25387ceabd6ab988a4aa3a9c060ae076e8a577e
Erhan <erhantezer68@gmail.com>
2023-02-08 21:54:35 +0300

    app js finished

--- 14-cart/src/App.js
+++ 14-cart/src/App.js
@@ -1,15 +1,29 @@
 import CartContainer from "./CartContainer";
+import { useGlobalContext } from "./context";
 import Navbar from "./Navbar";
 
 
 function App() {
-  
+  const { loading } = useGlobalContext();
+
+
+
   return (
-    <main>
-      <Navbar/>
-      <CartContainer/>
-    </main>
-  );
+    <>
+      {loading ? (
+        <div className='loading' >
+          <h1>Loading...</h1>
+        </div>
+      ) : (
+        <main>
+          <Navbar />
+          <CartContainer />
+        </main>
+      )
+      }
+    </>
+  )
+
 }
 
 export default App;
