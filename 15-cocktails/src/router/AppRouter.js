108f1a958da6a6bb44570bdb361b9fdfed37976f
Erhan <erhantezer68@gmail.com>
2023-02-09 05:49:38 +0300

    edited file

--- 15-cocktails/src/router/AppRouter.js
+++ 15-cocktails/src/router/AppRouter.js
@@ -1,10 +1,27 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Navbar from "../components/Navbar"
+import About from "../pages/About"
+import Error from "../pages/Error"
+import Home from "../pages/Home"
+import SingleCocktail from "../pages/SingleCocktail"
+import PrivateRouter from "./PrivateRouter"
 
 const AppRouter = () => {
 
-  return (
-    
-  )
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home/>} />
+                <Route path="about" element={<About/>} />
+                <Route path="cocktail/:id" element={<PrivateRouter/>} >
+                    <Route path="" element={<SingleCocktail/>} />
+                </Route>
+                <Route path='*' element={<Error />} />
+                
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default AppRouter
