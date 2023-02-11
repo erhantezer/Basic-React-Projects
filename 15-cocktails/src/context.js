2995874690931b31d66d19d99d74916c0735f115
Erhan <erhantezer68@gmail.com>
2023-02-11 23:15:29 +0300

    context edited and fetch api

--- 15-cocktails/src/context.js
+++ 15-cocktails/src/context.js
@@ -1,4 +1,4 @@
-import {createContext, useContext, useEffect, useState} from "react"
+import { createContext, useCallback, useContext, useEffect, useState } from "react"
 
 const AppContext = createContext();
 
@@ -8,29 +8,48 @@ export const useGlobalContext = () => {
 
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
-export const AppProvider = ({children}) => {
-const [loading, setLoading] = useState(false);
-const [searchTerm, setSearchTerm] = useState("a")
-const [cocktails, setCocktails] = useState([])
+export const AppProvider = ({ children }) => {
+    const [loading, setLoading] = useState(false);
+    const [searchTerm, setSearchTerm] = useState("a")
+    const [cocktails, setCocktails] = useState([])
 
 
-const fetchApı =  async () => {
-    try {
-        const res = await fetch(`${url}${searchTerm}`);
-        const data = await res.json()
-        console.log(data)
-    } catch (error) {
-        
-    }
-}
+    const fetchApı = useCallback(async () => {
+        setLoading(true);
+        try {
+            const res = await fetch(`${url}${searchTerm}`);
+            const data = await res.json()
+            console.log(data);
+            const { drinks } = data;
+            if (drinks) {
+                const newCocktails = drinks.map((item) => {
+                    const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } = item;
+                    return {
+                        id: idDrink,
+                        name: strDrink,
+                        image: strDrinkThumb,
+                        info: strAlcoholic,
+                        glass: strGlass,
+                    }
+                })
+                setCocktails(newCocktails)
+            } else {
+                setCocktails([])
+            }
+            setLoading(false)
+        } catch (error) {
+            console.log(error);
+            setLoading(false)
+        }
+    },[searchTerm])
 
-useEffect(() => {
-    fetchApı()
-}, [searchTerm]);
+    useEffect(() => {
+        fetchApı()
+    }, [searchTerm,fetchApı]);
 
     return (
-        <AppContext.Provider value={{}}>
+        <AppContext.Provider value={{loading, cocktails, searchTerm,setSearchTerm}}>
             {children}
         </AppContext.Provider>
-        )
+    )
 }
