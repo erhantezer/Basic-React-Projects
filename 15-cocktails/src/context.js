4172df970c10201505720fd068f9b3ada981780f
Erhan <erhantezer68@gmail.com>
2023-02-11 17:17:57 +0300

    context updated

--- 15-cocktails/src/context.js
+++ 15-cocktails/src/context.js
@@ -1,4 +1,4 @@
-import {createContext, useContext} from "react"
+import {createContext, useContext, useState} from "react"
 
 const AppContext = createContext();
 
@@ -6,9 +6,22 @@ export const useGlobalContext = () => {
     return useContext(AppContext);
 }
 
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
 export const AppProvider = ({children}) => {
+const [loading, setLoading] = useState(false);
+const [searchTerm, setSearchTerm] = useState("a")
+const [cocktails, setCocktails] = useState([])
 
+const fetchApı =  async () => {
+    try {
+        const res = await fetch(`${url}${searchTerm}`);
+        const data = await res.json()
+        console.log(data)
+    } catch (error) {
+        
+    }
+}
 
     return (
         <AppContext.Provider value={{}}>
