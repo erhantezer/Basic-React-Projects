dd17e30e1149cc66bd379764727fa7fd6cc1434a
Erhan <erhantezer68@gmail.com>
2023-02-12 00:16:13 +0300

    search form useref

--- 15-cocktails/src/components/SearchForm.js
+++ 15-cocktails/src/components/SearchForm.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import { useEffect, useRef } from 'react'
+import { useGlobalContext } from '../context'
 
 const SearchForm = () => {
+    const { setSearchTerm } = useGlobalContext()
+    const searchValue = useRef('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    useEffect(() => {
+        searchValue.current.focus();
+    })
+
     return (
-        <div>SearchForm</div>
+        <section className='section search'>
+            <form className='search-form' onSubmit={handleSubmit}>
+                <div className='form-control'>
+                    <label htmlFor="name">search your favorite cocktail</label>
+                    <input
+                        type="text"
+                        name="name"
+                        id='name'
+                        onChange={(e) => setSearchTerm(searchValue.current.value)}
+                    />
+                </div>
+            </form>
+        </section>
     )
 }
 
