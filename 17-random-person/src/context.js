b7aa4ce144a103a38f5e587d1d0ca3c22698ea36
Erhan <erhantezer68@gmail.com>
2023-02-12 23:39:38 +0300

    updated

--- 17-random-person/src/context.js
+++ 17-random-person/src/context.js
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 
 const AppContext = createContext();
@@ -8,11 +8,37 @@ export const useGlobalContext = () => {
 };
 
 
+const url = 'https://randomuser.me/api/'
+// const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg'
+
 export const AppProvider = ({children}) => {
+    const [loading, setLoading] = useState(false);
+    const [person, setPerson] = useState([]);
+    const [value, setValue] = useState('random person');
+    const [title, setTitle] = useState('name');
+
+    const getData = async () => {
+        setLoading(true)
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            console.log(data.results[0])
+            
+            setPerson(data.results[0])
+            setLoading(false)
+        } catch (error) {
+            console.log(error);
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        getData()
+    }, []);
 
 
     return (
-            <AppContext.Provider value={{}}>
+            <AppContext.Provider value={{loading, person}}>
                 {children}
             </AppContext.Provider>
         )
