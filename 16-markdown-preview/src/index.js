01c92ef68a70fd4aa3667e750ce5b8ea75b363dc
Erhan <erhantezer68@gmail.com>
2023-02-12 03:13:44 +0300

    updated

--- 16-markdown-preview/src/index.js
+++ 16-markdown-preview/src/index.js
@@ -3,5 +3,9 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>)
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
