5c1858d57c0c994e966bfa2d6685d97ec124dd61
Erhan <erhantezer68@gmail.com>
2023-02-13 23:24:05 +0300

    project finished

--- 20-dark-mode/src/App.js
+++ 20-dark-mode/src/App.js
@@ -1,20 +1,44 @@
 import Article from "./Article";
 import data from "./data";
-import {useState} from "react"
+import { useEffect, useState } from "react"
+
+
+const getStorageTheme = () => {
+  let theme = "light-theme";
+  if (localStorage.getItem("theme")) {
+    theme = localStorage.getItem("theme");
+  };
+  return theme;
+}
 
 function App() {
-  const [theme, setTheme] = useState("aaa");
+  const [theme, setTheme] = useState(
+    getStorageTheme()
+    // "light-theme"
+    );
 
-const toggleBtn = () => {
-  console.log(theme)
-}
+  const toggleTheme = () => {
+    if (theme === 'light-theme') {
+      setTheme("dark-theme")
+    } else {
+      setTheme('light-theme')
+    }
+    console.log(theme)
+  }
+  
+  // document.documentElement.classList = theme;
+
+  useEffect(() => {
+    document.documentElement.classList = theme;
+    localStorage.setItem('theme', theme);
+  },[theme])
 
   return (
     <main>
       <nav>
-      <div className="nav-center">
+        <div className="nav-center">
           <h1>overreacted</h1>
-          <button className="btn" onClick={toggleBtn}>
+          <button className="btn" onClick={toggleTheme}>
             toggle
           </button>
         </div>
