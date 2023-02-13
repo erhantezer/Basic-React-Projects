98d72b8e725a3bebfe1ba3309ff93d69b2fea9d0
Erhan <erhantezer68@gmail.com>
2023-02-13 22:45:04 +0300

    updated dark mode project

--- 20-dark-mode/src/App.js
+++ 20-dark-mode/src/App.js
@@ -1,10 +1,31 @@
-
+import Article from "./Article";
+import data from "./data";
+import {useState} from "react"
 
 function App() {
+  const [theme, setTheme] = useState("aaa");
+
+const toggleBtn = () => {
+  console.log(theme)
+}
+
   return (
-    <div className="App">
- 
-    </div>
+    <main>
+      <nav>
+      <div className="nav-center">
+          <h1>overreacted</h1>
+          <button className="btn" onClick={toggleBtn}>
+            toggle
+          </button>
+        </div>
+      </nav>
+      <section className="articles">
+        {data.map((item) => {
+          return <Article key={item.id} {...item} />;
+        })}
+      </section>
+
+    </main>
   );
 }
 
