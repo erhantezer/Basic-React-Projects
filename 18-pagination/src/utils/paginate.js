34aded10e6e2c8d0d968ca7b8aca35b2fe1bec06
Erhan <erhantezer68@gmail.com>
2023-02-13 18:01:47 +0300

    finished project

--- 18-pagination/src/utils/paginate.js
+++ 18-pagination/src/utils/paginate.js
@@ -4,12 +4,12 @@ const paginate = (followers) => {
     const itemsPerPage = 10;
     const numberOfPages = Math.ceil(followers.length / itemsPerPage);
 
-    const newFollowers = [{ length: numberOfPages }, (_, index) => {
+    const newFollowers = Array.from({ length: numberOfPages }, (_, index) => {
         const start = index * itemsPerPage
         return followers.slice(start, start + itemsPerPage)
 
-    }]
-
+    })
+    console.log(newFollowers)
     return newFollowers
 
 }
