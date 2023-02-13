d578d5391cb288db88f840de8678007f202f004a
Erhan <erhantezer68@gmail.com>
2023-02-13 04:56:35 +0300

    updated app js

--- 18-pagination/src/App.js
+++ 18-pagination/src/App.js
@@ -5,9 +5,28 @@ function App() {
  const {loading, data} = useFetch()
 
   return (
-    <div>
-fdgd
-    </div>
+<main>
+      <div className='section-title'>
+        <h1>{loading ? 'loading...' : 'pagination'}</h1>
+        <div className='underline'></div>
+      </div>
+      <section className='followers'>
+        <div className='container'>
+
+        </div>
+        {!loading && (
+          <div className='btn-container'>
+            <button className='prev-btn' >
+              prev
+            </button>
+            
+            <button className='next-btn' >
+              next
+            </button>
+          </div>
+        )}
+      </section>
+    </main>
   );
 }
 
