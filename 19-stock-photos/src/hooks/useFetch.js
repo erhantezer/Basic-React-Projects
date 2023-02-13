5a9f3122737559d0416116b782ea5d5864c7ce55
Erhan <erhantezer68@gmail.com>
2023-02-13 22:12:03 +0300

    mistake client id

---
+++ 19-stock-photos/src/hooks/useFetch.js
@@ -0,0 +1,21 @@
+// import React, { useState } from 'react'
+
+// const useFetch = () => {
+//     const [loading, setLoading] = useState(false);
+//     const [photos, setPhotos] = useState([]);
+
+//     const fetchImages = async () => {
+//         setLoading(true)
+//         try {
+//             const response = await fetch()
+//         } catch (error) {
+            
+//         }
+//     }
+
+//   return {}
+  
+  
+// }
+
+// export default useFetch
