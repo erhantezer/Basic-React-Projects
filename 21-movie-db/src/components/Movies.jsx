325dfd64976f579e8c440f4ac1e4378e93d315ff
Erhan <erhantezer68@gmail.com>
2023-02-14 16:41:05 +0300

    movies finished

--- 21-movie-db/src/components/Movies.jsx
+++ 21-movie-db/src/components/Movies.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import useFetch from "../useFetch";
+import {Link} from "react-router-dom"
+
+const url = 'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png';
+
 
 const Movies = () => {
+    const { loading, movies } = useFetch();
+
+
     return (
-        <div>Movies</div>
+        <>
+        { loading ? 
+        (<div className='loading'></div>) : 
+        (
+        <section className="movies">
+            {movies?.map((movie) => {
+            const { imdbID: id, Poster: poster, Title: title, Year: year } = movie
+                return(
+                    <Link to={`movies/${id}`} key={id} className="movie">
+                    <article>
+                        <img src={poster === 'N/A' ? url : poster} alt={title} />
+                        <div className="movie-info">
+                            <h4 className="title">{title}</h4>
+                            <p>{year}</p>
+                        </div>
+                    </article>
+                    </Link>
+                    )
+            })}
+        </section>
+        )}
+        </>
     )
 }
 
