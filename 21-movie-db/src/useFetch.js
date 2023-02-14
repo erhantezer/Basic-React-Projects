8c6cfaeafaeb0276910a83ac84e1c4d419f6c1cb
Erhan <erhantezer68@gmail.com>
2023-02-14 03:53:08 +0300

    customHook useFetch

--- 21-movie-db/src/useFetch.js
+++ 21-movie-db/src/useFetch.js
@@ -1,10 +1,25 @@
+import { useState } from "react";
 
+
+const url = ""
 const useFetch = () => {
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState([]);
 
+    const fetchData = async () => {
+        setLoading(true)
+        try {
+            const res = await fetch(url);
+            const data = await res.json()
+            setData(data)
+            setLoading(false)
+        } catch (error) {
+            console.log(error);
+            setLoading(false);
+        }
+    }
 
-
-
-    return {}
+    return { loading, data }
 
 }
 
