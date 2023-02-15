6fc08a7555e64753305cee872beaaa5997220b6a
Erhan <erhantezer68@gmail.com>
2023-02-15 22:17:00 +0300

    updated

--- 22-hacker-news/src/reducer.js
+++ 22-hacker-news/src/reducer.js
@@ -1,23 +1,32 @@
-import { SET_LOADING, SET_STORIES, REMOVE_STORY, HANDLE_PAGE, HANDLE_SEARCH } from "./actions";
-
-
+import {
+  SET_LOADING,
+  SET_STORIES,
+  REMOVE_STORY,
+  HANDLE_PAGE,
+  HANDLE_SEARCH,
+} from "./actions"
 
 export const reducer = (state, action) => {
+  switch (action.type) {
+    case SET_LOADING:
+      return {...state, isLoading: true}
+    case SET_STORIES:
+      return {
+        ...state,
+        isLoading: false,
+        hits: action.payload.hits,
+        nbPages: action.payload.nbPages,
+      }
+    case REMOVE_STORY:
+      return {...state}
+    case HANDLE_PAGE:
+      if (action.payload === "inc") {
+      }
+      return {...state}
+    case HANDLE_SEARCH:
+      return {...state, query: action.payload, page: 0}
 
-    switch (action.type) {
-
-        case SET_LOADING:
-            return { ...state, isLoading: true }
-        case SET_STORIES:
-            return { ...state, isLoading: false, hits: action.payload.hits, nbPages: action.payload.nbPages }
-        case REMOVE_STORY:
-            return { ...state, }
-        case HANDLE_PAGE:
-            return { ...state, }
-        case HANDLE_SEARCH:
-            return { ...state, }
-
-        default:
-            return state
-    }
+    default:
+      return state
+  }
 }
