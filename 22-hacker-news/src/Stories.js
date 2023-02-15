d016a457a50a8df894d7e3b1ed13265ab491429a
Erhan <erhantezer68@gmail.com>
2023-02-15 23:57:58 +0300

    stories loading added

--- 22-hacker-news/src/Stories.js
+++ 22-hacker-news/src/Stories.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { useGlobalContext } from './context';
 
 const Stories = () => {
-	return <div>Stories</div>;
+  const {isLoading, hits, removeStory} = useGlobalContext();
+	return (
+    <>
+    {isLoading ? (<div className='loading'></div>)
+    
+    :(
+    <section>
+      
+    </section>
+    )}
+    </>
+    )
 };
 
 export default Stories;
