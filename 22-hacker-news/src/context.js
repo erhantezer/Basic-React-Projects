ae178a0d1bad26a22c62a88b5b05202945435be6
Erhan <erhantezer68@gmail.com>
2023-02-15 17:15:56 +0300

    updated

--- 22-hacker-news/src/context.js
+++ 22-hacker-news/src/context.js
@@ -1,15 +1,6 @@
-import { createContext, useContext } from "react";
-
-
-const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?';
-
-const initialState = {
-    isLoading: true,
-    hits: [],
-    query: "react",
-    page: 0,
-    nbPages: 0,
-} 
+import { createContext, useContext, useReducer } from "react";
+import { reducer } from "./reducer";
+import { SET_LOADING, SET_STORIES } from "./actions";
 
 
 const AppContext = createContext();
@@ -18,11 +9,33 @@ export const useGlobalContext = () => {
     return useContext(AppContext)
 };
 
+const initialState = {
+    isLoading: true,
+    hits: [],
+    query: "react",
+    page: 0,
+    nbPages: 0,
+}
+
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?';
+
 export const AppProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    const fetchStories = async (url) => {
+        dispatch({ type: SET_LOADING });
+        try {
+            const res = await fetch(url);
+            const data = await res.json()
+            dispatch({ type: SET_STORIES, payload: { hits: data.hits, nbPages: data.nbPages } })
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
 
     return (
-        <AppContext.Provider>
+        <AppContext.Provider value={{...state}}>
             {children}
         </AppContext.Provider>
     )
--- 22-hacker-news/src/reducer.js
+++ 22-hacker-news/src/reducer.js
@@ -2,14 +2,14 @@ import { SET_LOADING, SET_STORIES, REMOVE_STORY, HANDLE_PAGE, HANDLE_SEARCH } fr
 
 
 
-export const reduver = (state, action) => {
+export const reducer = (state, action) => {
 
     switch (action.type) {
 
         case SET_LOADING:
-            return { ...state, }
+            return { ...state, isLoading: true }
         case SET_STORIES:
-            return { ...state, }
+            return { ...state, isLoading: false, hits: action.payload.hits, nbPages: action.payload.nbPages }
         case REMOVE_STORY:
             return { ...state, }
         case HANDLE_PAGE:
