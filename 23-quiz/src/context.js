6b33a9f7576768c65a106c7abcbc6f504fe8dd84
Erhan <erhantezer68@gmail.com>
2023-02-16 21:37:47 +0300

    updated and finished

--- 23-quiz/src/context.js
+++ 23-quiz/src/context.js
@@ -1,10 +1,5 @@
-import {createContext, useContext, useEffect, useState} from 'react';
 import axios from 'axios';
-const AppContext = createContext();
-
-export const useGlobalContext = () => {
-	return useContext(AppContext);
-};
+import React, {useState, useContext} from 'react';
 
 const table = {
 	sports: 21,
@@ -13,46 +8,118 @@ const table = {
 };
 
 const API_ENDPOINT = 'https://opentdb.com/api.php?';
-const url = '';
-const tempUrl =
-	'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple';
 
-export const AppProvider = ({children}) => {
+// const url = '';
+// const tempUrl =
+// 	'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple';
+const AppContext = React.createContext();
+
+const AppProvider = ({children}) => {
+	const [waiting, setWaiting] = useState(true);
 	const [loading, setLoading] = useState(false);
-	const [question, setQuestion] = useState([]);
+	const [questions, setQuestions] = useState([]);
+	const [index, setIndex] = useState(0);
+	const [correct, setCorrect] = useState(0);
+	const [error, setError] = useState(false);
+	const [isModalOpen, setIsModalOpen] = useState(false);
 	const [quiz, setQuiz] = useState({
 		amount: 10,
 		category: 'sports',
-		difficult: 'easy',
+		difficulty: 'easy',
 	});
+	
 
-	const fetchQuiz = async (url) => {
-		setLoading(true)
-		try {
-			const {data} = await axios(url);
-			const veri = data.results
-			console.log(veri);
-			setQuestion(veri)
-			setLoading(false)
-		} catch (error) {
-			console.log(error);
-			setLoading(false)
+	const fetchQuestions = async (url) => {
+		setLoading(true);
+		setWaiting(false);
+		const response = await axios(url).catch((err) => console.log(err));
+
+		if (response) {
+			const data = response.data.results;
+			console.log(response.data.results);
+			if (data.length > 0) {
+				setQuestions(data);
+				setLoading(false);
+				setWaiting(false);
+				setError(false);
+			} else {
+				setWaiting(true);
+				setError(true);
+			}
+
+		} else {
+			setWaiting(true);
 		}
 	};
 
-	useEffect(() => {
-		const {amount, category, difficulty} = quiz;
-		const url = `${API_ENDPOINT}amount=${amount}&difficult=${difficulty}&category=${table[category]}&type=multiple`;
-		fetchQuiz(url);
-	}, []);
+	const nextQuestion = () => {
+		setIndex((oldIndex) => {
+			const index = oldIndex + 1;
+			if (index > questions.length - 1) {
+				openModal();
+				return 0;
+			} else {
+				return index;
+			}
+		});
+	};
+
+	const checkAnswer = (value) => {
+		if (value) {
+			setCorrect((oldState) => oldState + 1);
+		}
+		nextQuestion();
+	};
+
+	const openModal = () => {
+		setIsModalOpen(true);
+	};
+
+	const closeModal = () => {
+		setWaiting(true);
+		setCorrect(0);
+		setIsModalOpen(false);
+	};
+
+	const handleChange = (e) => {
+		const name = e.target.name;
+		const value = e.target.value;
+		setQuiz({...quiz, [name]: value});
+	};
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		const {amount, category, difficulty} = quiz;
-		const url = `${API_ENDPOINT}amount=${amount}&difficult=${difficulty}&category=${table[category]}&type=multiple`;
-		fetchQuiz(url);
+
+		const url = `${API_ENDPOINT}amount=${amount}&difficulty=${difficulty}&category=${table[category]}&type=multiple`;
+		fetchQuestions(url);
 	};
+
 	return (
-		<AppContext.Provider value={{handleSubmit}}>{children}</AppContext.Provider>
+		<AppContext.Provider
+			value={{
+				waiting,
+				loading,
+				questions,
+				index,
+				correct,
+				error,
+				isModalOpen,
+				nextQuestion,
+				checkAnswer,
+				closeModal,
+				quiz,
+				handleChange,
+				handleSubmit,
+			}}
+		>
+			{children}
+		</AppContext.Provider>
 	);
 };
+// make sure use
+export const useGlobalContext = () => {
+	return useContext(AppContext);
+};
+
+export {AppContext, AppProvider};
