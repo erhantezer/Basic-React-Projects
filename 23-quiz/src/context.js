2dd04c416567c8d40c6191a150ed191ef7cd9b9d
Erhan <erhantezer68@gmail.com>
2023-02-16 15:01:52 +0300

    data fetched

--- 23-quiz/src/context.js
+++ 23-quiz/src/context.js
@@ -1,10 +1,10 @@
-import {createContext, useContext} from 'react';
-
+import {createContext, useContext, useEffect, useState} from 'react';
+import axios from 'axios';
 const AppContext = createContext();
 
 export const useGlobalContext = () => {
-    return useContext(AppContext)
-}
+	return useContext(AppContext);
+};
 
 const table = {
 	sports: 21,
@@ -18,8 +18,41 @@ const tempUrl =
 	'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple';
 
 export const AppProvider = ({children}) => {
+	const [loading, setLoading] = useState(false);
+	const [question, setQuestion] = useState([]);
+	const [quiz, setQuiz] = useState({
+		amount: 10,
+		category: 'sports',
+		difficult: 'easy',
+	});
+
+	const fetchQuiz = async (url) => {
+		setLoading(true)
+		try {
+			const {data} = await axios(url);
+			const veri = data.results
+			console.log(veri);
+			setQuestion(veri)
+			setLoading(false)
+		} catch (error) {
+			console.log(error);
+			setLoading(false)
+		}
+	};
+
+	useEffect(() => {
+		const {amount, category, difficulty} = quiz;
+		const url = `${API_ENDPOINT}amount=${amount}&difficult=${difficulty}&category=${table[category]}&type=multiple`;
+		fetchQuiz(url);
+	}, []);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const {amount, category, difficulty} = quiz;
+		const url = `${API_ENDPOINT}amount=${amount}&difficult=${difficulty}&category=${table[category]}&type=multiple`;
+		fetchQuiz(url);
+	};
 	return (
-    <AppContext.Provider value={{}}>
-        {children}
-    </AppContext.Provider>)
+		<AppContext.Provider value={{handleSubmit}}>{children}</AppContext.Provider>
+	);
 };
